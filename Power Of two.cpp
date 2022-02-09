d89d6afcdaf925dab6ba502a35ca6f91477741af
thatsme10 <75313778+thatsme10@users.noreply.github.com>
2022-02-09 18:12:45 +0530

    Create Power Of two.cpp

---
+++ Power Of two.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here  
+        int k=-1;
+        bool b=true;
+        
+        if(n==0)
+        return false;
+        
+        for(int i=31;i>=-1;i--)
+        {
+            
+            int ind=n>>i;
+            if(ind&1==1)
+            {
+                k++;
+                
+            }
+            
+            if(k==1)
+            b=false;
+            
+            
+            
+        }
+        return b;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
